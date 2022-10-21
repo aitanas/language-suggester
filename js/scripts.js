b6ece4d1488280b8df5fe8c9c79221ca00af0488
Aitana Shough <aeshough@gmail.com>
2022-10-21 11:04:55 -0700

    WIP: add javascript converting form values to const variables

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,25 @@
 // program: websites, apps, iOS, bees
 // oneFear: birds, snakes, words
 // colors: hexadecimal code stuff?? RNG? convert hex code to integer, number thresholds? >=1000, >9000
-const hexInput = document.getElementById("colorPick").value;
+function getResult (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const whatProgram = document.querySelector("input[name='whatProgram']:checked").value;
+  const whyProgram = document.querySelector("input[name='whyProgram']:checked").value;
+  const oneFear= document.querySelector("input[name='oneFear']:checked").value;
+  const hexInput = document.getElementById("colorPick").value;
 
-// converts hexadecimal to float
-function convertHex (hexInput) {
-let convertedHex = hexInput.slice(-5);
-convertedHex = Math.floor(parseInt(convertedHex, 16)) / 1000;
-return convertedHex
+  // converts hexadecimal to float
+  function convertHex (hexInput) {
+  let convertedHex = hexInput.slice(-5);
+  convertedHex = Math.floor(parseInt(convertedHex, 16)) / 1000;
+  return convertedHex
+  }
+
+  document.getElementById("nameHere").innerText = result;
+
+
+  window.addEventListener("load", function() {
+    document.getElementById("form").addEventListener("submit", getLength);
+  });
 }
