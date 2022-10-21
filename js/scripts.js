51c24a23eb5c9f9fa9eead1ac25dc2a258cd78f9
Aitana Shough <aeshough@gmail.com>
2022-10-21 12:01:41 -0700

    remove hex code parameter as it made results too fine-tuned

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,14 @@ function getResult (event) {
   const name = document.getElementById("name").value;
   const whatProgram = document.querySelector("input[name='whatProgram']:checked").value;
   const whyProgram = document.querySelector("input[name='whyProgram']:checked").value;
-  const oneFear= document.querySelector("input[name='oneFear']:checked").value;
+  const oneFear = document.querySelector("input[name='oneFear']:checked").value;
   const hexInput = document.getElementById("colorPick").value;
   // converts hexadecimal to float
   let convertedHex = hexInput.slice(-5); // removes # from hexadecimal input
   convertedHex = Math.floor(parseInt(convertedHex, 16)) / 1000; // makes converted hex a float value 0-~1500
   convertedHex = parseInt(convertedHex);
 
-  document.getElementById("nameHere").innerText = result;
+  let nameResult; 
 
 // branching logic
 // program: websites, apps, iOS, bees
@@ -28,18 +28,23 @@ function getResult (event) {
 // oneFear: birds, snakes, words
 // colors: number thresholds, i.e. >=1000, >9000
 if (name && whatProgram && whyProgram && oneFear && convertedHex) {
-  if (whatProgram === websites && whyProgram && oneFear === snakes || birds && convertedHex >=500) {
+  if (whatProgram === webpages || bees && whyProgram && oneFear === snakes || birds) {
     document.getElementById("javascriptResult").removeAttribute("class");
-  } else if (whatProgram === iOs && whyProgram && oneFear === words || snakes && convertedHex <= 1000) {
+    nameResult = name;
+  } else if (whatProgram === iOS && whyProgram && oneFear === words || snakes) {
     document.getElementById("swiftResult").removeAttribute("class");
-  } else if (whatProgram === apps && whyProgram && oneFear === snakes && !convertedHex || convertedHex < 500) {
+    nameResult = name;
+  } else if (whatProgram === apps || bees && whyProgram && oneFear === snakes) {
     document.getElementById("csharpResult").removeAttribute("class");
-  } else if (whatProgram === apps || bees && whyProgram && oneFear === !snakes && convertedHex ) {
-    document.getElementById("javascriptResult").removeAttribute("class");
+    nameResult = name;
+  } else if (whatProgram === apps || bees && whyProgram && oneFear != snakes) {
+    document.getElementById("pythonResult").removeAttribute("class");
+    nameResult = name;
   }
 } else {
   document.getElementById("errorResult").removeAttribute("class");
 }
+  document.getElementById("nameHere").innerText = nameResult;
 }
 
 window.addEventListener("load", function() {
